417ba72916762796fbc9462f8a8cd6bdcc622b08
annasmagina <anna@ptf-lab.com>
2024-11-18 06:50:39 +0100

    Fixed datapaths

--- src/soccernet_calibration/baseline_cameras.py
+++ src/soccernet_calibration/baseline_cameras.py
@@ -132,18 +132,37 @@ def draw_pitch_homography(image, homography):
     return image
 
 
+def calculate_reprojection_error(homography, line_matches):
+    """
+    Calculates the reprojection error for the given homography and line matches.
+    :param homography: The homography matrix.
+    :param line_matches: List of pairs of 2D lines matches.
+    :return: The reprojection error.
+    """
+    total_error = 0
+    for line_pair in line_matches:
+        src_line = line_pair[0]
+        target_line = line_pair[1]
+
+        reprojected_line = np.transpose(np.linalg.inv(homography)) @ src_line
+        reprojected_line /= np.linalg.norm(reprojected_line[:2])
+        target_line /= np.linalg.norm(target_line[:2])
+        error = np.abs(np.dot(reprojected_line, target_line))
+        total_error += error
+
+    return total_error / len(line_matches)
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description='Baseline for camera parameters extraction')
 
-    parser.add_argument('-s', '--soccernet', default="/home/fmg/data/SN23/calibration-2023-bis/", type=str,
+    parser.add_argument('-i', '--images', default="/home/fmg/data/SN23/calibration-2023-bis/", type=str,
                         help='Path to the SoccerNet-V3 dataset folder')
     parser.add_argument('-p', '--prediction', default="/home/fmg/results/SN23-tests/",
                         required=False, type=str,
                         help="Path to the prediction folder")
-    parser.add_argument('--split', required=False, type=str,
-                        default="valid", help='Select the split of data')
     parser.add_argument('--resolution_width', required=False, type=int, default=960,
                         help='width resolution of the images')
     parser.add_argument('--resolution_height', required=False, type=int, default=540,
@@ -152,78 +171,89 @@ if __name__ == "__main__":
 
     field = SoccerPitch()
 
-    dataset_dir = os.path.join(args.soccernet, args.split)
+    dataset_dir = os.path.join(args.images)
     if not os.path.exists(dataset_dir):
         print("Invalid dataset path !")
         exit(-1)
 
-    with open(os.path.join(dataset_dir, "per_match_info.json"), 'r') as f:
-        match_info = json.load(f)
+    images_dir = args.images
+    if not os.path.exists(images_dir):
+        print("Invalid images directory path !")
+        exit(-1)
 
-    with tqdm(enumerate(match_info.keys()), total=len(match_info.keys()), ncols=160) as t:
-        for i, match in t:
-            frame_list = match_info[match].keys()
+    prediction_files = [f for f in sorted(
+        os.listdir(args.prediction)) if ".json" in f]
 
-            for frame in frame_list:
-                frame_index = frame.split(".")[0]
-                prediction_file = os.path.join(
-                    args.prediction, args.split, f"extremities_{frame_index}.json")
+    with tqdm(enumerate(prediction_files), total=len(prediction_files), ncols=160) as t:
+        for frame in prediction_files:
+            frame_index = frame.split(".")[0]
+            prediction_file = os.path.join(
+                args.prediction, f"{frame_index}.json")
 
-                if not os.path.exists(prediction_file):
+            if not os.path.exists(prediction_file):
+                continue
+
+            with open(prediction_file, 'r') as f:
+                predictions = json.load(f)
+
+            camera_predictions = dict()
+            # image_path = os.path.join(dataset_dir, frame.stem + ".jpg")
+            # cv_image = cv.imread(image_path)
+            # cv_image = cv.resize(cv_image, (args.resolution_width, args.resolution_height))
+
+            line_matches = []
+            potential_3d_2d_matches = {}
+            src_pts = []
+            success = False
+            for k, v in predictions.items():
+                if 'Circle' in k or "unknown" in k or 'Goal' in k:
                     continue
+                P3D1 = field.line_extremities_keys[k][0]
+                P3D2 = field.line_extremities_keys[k][1]
+                p1 = np.array([v[0]['x'] * args.resolution_width,
+                              v[0]['y'] * args.resolution_height, 1.])
+                p2 = np.array([v[1]['x'] * args.resolution_width,
+                              v[1]['y'] * args.resolution_height, 1.])
+                src_pts.extend([p1, p2])
+                if P3D1 in potential_3d_2d_matches.keys():
+                    potential_3d_2d_matches[P3D1].extend([p1, p2])
+                else:
+                    potential_3d_2d_matches[P3D1] = [p1, p2]
+                if P3D2 in potential_3d_2d_matches.keys():
+                    potential_3d_2d_matches[P3D2].extend([p1, p2])
+                else:
+                    potential_3d_2d_matches[P3D2] = [p1, p2]
 
-                with open(prediction_file, 'r') as f:
-                    predictions = json.load(f)
+                start = (int(p1[0]), int(p1[1]))
+                end = (int(p2[0]), int(p2[1]))
+                # cv.line(cv_image, start, end, (0, 0, 255), 1)
 
-                camera_predictions = dict()
-                image_path = os.path.join(dataset_dir, frame)
-                # cv_image = cv.imread(image_path)
-                # cv_image = cv.resize(cv_image, (args.resolution_width, args.resolution_height))
+                line = np.cross(p1, p2)
+                if np.isnan(np.sum(line)) or np.isinf(np.sum(line)):
+                    continue
+                line_pitch = field.get_2d_homogeneous_line(k)
+                if line_pitch is not None:
+                    line_matches.append((line_pitch, line))
 
-                line_matches = []
-                potential_3d_2d_matches = {}
-                src_pts = []
-                success = False
-                for k, v in predictions.items():
-                    if k == 'Circle central' or "unknown" in k:
-                        continue
-                    P3D1 = field.line_extremities_keys[k][0]
-                    P3D2 = field.line_extremities_keys[k][1]
-                    p1 = np.array([v[0]['x'] * args.resolution_width,
-                                  v[0]['y'] * args.resolution_height, 1.])
-                    p2 = np.array([v[1]['x'] * args.resolution_width,
-                                  v[1]['y'] * args.resolution_height, 1.])
-                    src_pts.extend([p1, p2])
-                    if P3D1 in potential_3d_2d_matches.keys():
-                        potential_3d_2d_matches[P3D1].extend([p1, p2])
-                    else:
-                        potential_3d_2d_matches[P3D1] = [p1, p2]
-                    if P3D2 in potential_3d_2d_matches.keys():
-                        potential_3d_2d_matches[P3D2].extend([p1, p2])
-                    else:
-                        potential_3d_2d_matches[P3D2] = [p1, p2]
-
-                    start = (int(p1[0]), int(p1[1]))
-                    end = (int(p2[0]), int(p2[1]))
-                    # cv.line(cv_image, start, end, (0, 0, 255), 1)
-
-                    line = np.cross(p1, p2)
-                    if np.isnan(np.sum(line)) or np.isinf(np.sum(line)):
-                        continue
-                    line_pitch = field.get_2d_homogeneous_line(k)
-                    if line_pitch is not None:
-                        line_matches.append((line_pitch, line))
-
-                if len(line_matches) >= 4:
-                    target_pts = [field.point_dict[k][:2]
-                                  for k in potential_3d_2d_matches.keys()]
-                    T1 = normalization_transform(target_pts)
-                    T2 = normalization_transform(src_pts)
-                    success, homography = estimate_homography_from_line_correspondences(
-                        line_matches, T1, T2)
-                    if success:
-                        # cv_image = draw_pitch_homography(cv_image, homography)
-
+            if len(line_matches) >= 4:
+                target_pts = [field.point_dict[k][:2]
+                              for k in potential_3d_2d_matches.keys()]
+                T1 = normalization_transform(target_pts)
+                T2 = normalization_transform(src_pts)
+                success, homography = estimate_homography_from_line_correspondences(
+                    line_matches, T1, T2)
+                reprojection_error = calculate_reprojection_error(
+                    homography, line_matches)
+                homography_file = os.path.join(
+                    args.prediction, f"homography_{frame_index}.json")
+                homography_dict = {"success": int(success),
+                                   "reprojection_error": reprojection_error,
+                                   "homography": homography.tolist()}
+                with open(homography_file, 'w') as f:
+                    json.dump(homography_dict, f, indent=4)
+                if success:
+                    # cv_image = draw_pitch_homography(cv_image, homography)
+                    try:
                         cam = Camera(args.resolution_width,
                                      args.resolution_height)
                         success = cam.from_homography(homography)
@@ -252,11 +282,13 @@ if __name__ == "__main__":
                                 # cv.imshow("colorful pitch", cv_image)
                                 # cv.waitKey(0)
 
-                if success:
-                    camera_predictions = cam.to_json_parameters()
+                            camera_predictions = cam.to_json_parameters()
 
-                task2_prediction_file = os.path.join(
-                    args.prediction, args.split, f"camera_{frame_index}.json")
-                if camera_predictions:
-                    with open(task2_prediction_file, "w") as f:
-                        json.dump(camera_predictions, f, indent=4)
+                            task2_prediction_file = os.path.join(
+                                args.prediction, f"camera_{frame_index}.json")
+
+                            if camera_predictions:
+                                with open(task2_prediction_file, "w") as f:
+                                    json.dump(camera_predictions, f, indent=4)
+                    except Exception as e:
+                        pass
